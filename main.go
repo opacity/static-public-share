164845044c302cb2dc79090c89112388cae15e44
Mihai Iorga <mihai7221@gmail.com>
2021-04-19 18:47:23 +0300

    remove storage-node request dependency

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 	"os"
@@ -9,11 +8,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
-
-type shortlinkFileResp struct {
-	URL          string `json:"url"`
-	ThumbnailURL string `json:"thumbnailUrl"`
-}
 
 func init() {
 	if err := godotenv.Load(); err != nil {
@@ -37,7 +31,6 @@ func getShortlink(c *gin.Context) {
 		c.AbortWithStatusJSON(http.StatusInternalServerError, err.Error())
 	}
 
-	shortlinkBackend, err := getShortlinkStorageNode(shortlink)
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusInternalServerError, err.Error())
 	}
@@ -46,14 +39,10 @@ func getShortlink(c *gin.Context) {
 		"Url":         os.Getenv("OPACITY_URL"),
 		"Title":       ps.Title,
 		"Description": ps.Description,
-		"Image":       shortlinkBackend.ThumbnailURL,
+		"Image":       getPublicShareThumbnailURL(ps.FileID),
 	})
 }
 
-func getShortlinkStorageNode(shortlink string) (shortlinkFileResp shortlinkFileResp, err error) {
-	url := os.Getenv("STORAGE_NODE_URL") + "api/v2/public-share/" + shortlink
-	req, err := http.Get(url)
-
-	json.NewDecoder(req.Body).Decode(&shortlinkFileResp)
-	return
+func getPublicShareThumbnailURL(fileHandle string) string {
+	return os.Getenv("NODE_BUCKET_URL") + fileHandle + "/thumbnail"
 }
