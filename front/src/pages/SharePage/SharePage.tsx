61b600e3bd7910100b1c1bdb29a960690a755dd4
supermavens <sunavenue.ca@gmail.com>
2021-05-31 12:22:16 -0700

    update ui

--- front/src/pages/SharePage/SharePage.tsx
+++ front/src/pages/SharePage/SharePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import SiteWrapper from "../../SiteWrapper";
 import { Row, Col, Container } from "react-bootstrap";
 import "./SharePage.scss";
