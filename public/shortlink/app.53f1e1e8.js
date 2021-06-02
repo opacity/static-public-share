cfbdbdae497209d8a71c1da20d3d7ae807912c6f
supermavens <sunavenue.ca@gmail.com>
2021-06-02 12:08:55 -0700

    testing telegram

--- public/shortlink/app.53f1e1e8.js
+++ public/shortlink/app.53f1e1e8.js
@@ -41,15 +41,15 @@ react_dom_1.render(react_1.createElement(App, null), document.getElementById("ro
 exports.__esModule = true;
 var react_1 = __fusereq(2);
 var react_1d = __fuse.dt(react_1);
-var SiteWrapper_1 = __fusereq(9);
+var SiteWrapper_1 = __fusereq(8);
 var SiteWrapper_1d = __fuse.dt(SiteWrapper_1);
-var react_bootstrap_1 = __fusereq(10);
-var config_1 = __fusereq(12);
-var preview_1 = __fusereq(13);
-var file_saver_1 = __fusereq(14);
-var react_loading_1 = __fusereq(15);
+var react_bootstrap_1 = __fusereq(9);
+var config_1 = __fusereq(11);
+var preview_1 = __fusereq(12);
+var file_saver_1 = __fusereq(13);
+var react_loading_1 = __fusereq(14);
 var react_loading_1d = __fuse.dt(react_loading_1);
-const shareImg = __fusereq(8);
+const shareImg = __fusereq(7);
 const SharePage = ({history}) => {
   const file = react_1.useMemo(() => window.OpacityFile, [window.OpacityFile]);
   const [pageLoading, setPageLoading] = react_1.useState(false);
@@ -119,18 +119,18 @@ const SharePage = ({history}) => {
 exports.default = SharePage;
 
 },
-8: function(__fusereq, exports, module){
+7: function(__fusereq, exports, module){
 module.exports = "/resources/1a6475db.svg";
 },
-9: function(__fusereq, exports, module){
+8: function(__fusereq, exports, module){
 exports.__esModule = true;
 var react_1 = __fusereq(2);
-var tabler_react_1 = __fusereq(17);
-var aos_1 = __fusereq(18);
+var tabler_react_1 = __fusereq(18);
+var aos_1 = __fusereq(19);
 var aos_1d = __fuse.dt(aos_1);
-var footer_1 = __fusereq(19);
+var footer_1 = __fusereq(20);
 var footer_1d = __fuse.dt(footer_1);
-var config_1 = __fusereq(12);
+var config_1 = __fusereq(11);
 aos_1d.default.init({
   once: true,
   anchorPlacement: "center-bottom",
@@ -138,7 +138,7 @@ aos_1d.default.init({
   delay: 50,
   duration: 700
 });
-const logo = __fusereq(16);
+const logo = __fusereq(17);
 class SiteWrapper extends react_1.Component {
   constructor(props) {
     super(props);
@@ -285,19 +285,19 @@ class SiteWrapper extends react_1.Component {
 exports.default = SiteWrapper;
 
 },
-12: function(__fusereq, exports, module){
+11: function(__fusereq, exports, module){
 exports.__esModule = true;
 exports.HOME_URL = "https://dev2.opacity.io";
 
 },
-13: function(__fusereq, exports, module){
+12: function(__fusereq, exports, module){
 exports.__esModule = true;
 var react_1 = __fusereq(2);
 var react_1d = __fuse.dt(react_1);
-var preview_renderer_1 = __fusereq(97);
-var react_markdown_1 = __fusereq(98);
+var preview_renderer_1 = __fusereq(22);
+var react_markdown_1 = __fusereq(23);
 var react_markdown_1d = __fuse.dt(react_markdown_1);
-var react_file_icon_1 = __fusereq(99);
+var react_file_icon_1 = __fusereq(24);
 const getTypeFromExt = ext => {
   ext = ("" + ext).replace(/^\./, "");
   if (["png", "apng", "svg", "gif", "bmp", "ico", "cur", "jpg", "jpeg", "jfif", "pjpeg", "pjp", "webp"].includes(ext)) {
@@ -379,10 +379,10 @@ exports.getTypeFromExt = getTypeFromExt;
 exports.Preview = Preview;
 
 },
-16: function(__fusereq, exports, module){
+17: function(__fusereq, exports, module){
 module.exports = "/resources/06dfaeef0.svg";
 },
-97: function(__fusereq, exports, module){
+22: function(__fusereq, exports, module){
 exports.__esModule = true;
 var react_1 = __fusereq(2);
 var react_1d = __fuse.dt(react_1);
