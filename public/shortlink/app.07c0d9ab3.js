90724c08b8ce0b1f192728f1688a0b89c9d5b16d
Faizan Baig <faizan@firekamp.com>
2022-05-12 23:56:20 +0530

    prod build

--- public/shortlink/app.07c0d9ab3.js
+++ public/shortlink/app.07c0d9ab3.js
@@ -41,14 +41,14 @@ react_dom_1.render(react_1.createElement(App, null), document.getElementById("ro
 exports.__esModule = true;
 var react_1 = __fusereq(2);
 var react_1d = __fuse.dt(react_1);
-var SiteWrapper_1 = __fusereq(8);
+var SiteWrapper_1 = __fusereq(7);
 var SiteWrapper_1d = __fuse.dt(SiteWrapper_1);
-var react_bootstrap_1 = __fusereq(9);
-var preview_1 = __fusereq(11);
-var file_saver_1 = __fusereq(12);
-var react_loading_1 = __fusereq(13);
+var react_bootstrap_1 = __fusereq(8);
+var preview_1 = __fusereq(10);
+var file_saver_1 = __fusereq(11);
+var react_loading_1 = __fusereq(12);
 var react_loading_1d = __fuse.dt(react_loading_1);
-var a404Page_1 = __fusereq(14);
+var a404Page_1 = __fusereq(13);
 var a404Page_1d = __fuse.dt(a404Page_1);
 const SharePage = ({history}) => {
   const file = react_1.useMemo(() => window.OpacityFile, [window.OpacityFile]);
@@ -128,7 +128,7 @@ const SharePage = ({history}) => {
 exports.default = SharePage;
 
 },
-8: function(__fusereq, exports, module){
+7: function(__fusereq, exports, module){
 exports.__esModule = true;
 var react_1 = __fusereq(2);
 var tabler_react_1 = __fusereq(16);
@@ -287,7 +287,7 @@ class SiteWrapper extends react_1.Component {
 exports.default = SiteWrapper;
 
 },
-11: function(__fusereq, exports, module){
+10: function(__fusereq, exports, module){
 exports.__esModule = true;
 var react_1 = __fusereq(2);
 var react_1d = __fuse.dt(react_1);
@@ -369,11 +369,11 @@ exports.getTypeFromExt = getTypeFromExt;
 exports.Preview = Preview;
 
 },
-14: function(__fusereq, exports, module){
+13: function(__fusereq, exports, module){
 exports.__esModule = true;
 var react_1 = __fusereq(2);
 var tabler_react_1 = __fusereq(16);
-var SiteWrapper_1 = __fusereq(8);
+var SiteWrapper_1 = __fusereq(7);
 var SiteWrapper_1d = __fuse.dt(SiteWrapper_1);
 const logo = __fusereq(100);
 const Page404 = ({history}) => {
